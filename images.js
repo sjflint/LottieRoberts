ec0a9cc19ebf21a6024da9a2a005bbb1f4a98f3a
Simon Flint <info@yorkkarate.net>
2021-02-20 14:15:28 +0000

    update 1.1

--- images.js
+++ images.js
@@ -1,5 +1,25 @@
-document.body.addEventListener('mouseover', function(e) {
-  if(e.target.classList.contains('services-img')){
-    console.log('moused over image')
+document.body.addEventListener('mouseout', function(e) {
+  if(e.target.classList.contains('overlay-image')){
+    e.target.classList.add('overlay-image-none')
+    e.target.classList.remove('overlay-image-hide')
+    e.target.nextElementSibling.classList.add('image-tagline')
+    e.target.nextElementSibling.classList.remove('tagline-show')
   }
 })
+
+// document.body.addEventListener('mouseout', function(e) {
+//   if(e.target.classList.contains('image-text'))
+// })
+
+document.body.addEventListener('mouseover', function(e) {
+  if(e.target.classList.contains('overlay-image') || e.target.classList.contains('image-text')){
+    e.target.classList.add('overlay-image-hide')
+    e.target.classList.remove('overlay-image-none')
+    e.target.nextElementSibling.classList.add('tagline-show')
+    e.target.nextElementSibling.classList.remove('image-tagline')
+  }
+})
+
+
+
+
