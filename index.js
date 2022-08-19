42fe9346189da15686b26284bf959cf337ae75ca
Simon Flint <simonflint@hotmail.co.uk>
2022-08-19 22:26:44 +0100

    test .env removed

--- index.js
+++ index.js
@@ -199,3 +199,5 @@ app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// test
